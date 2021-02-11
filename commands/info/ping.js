cae08a8666e0d054cd1c5c3f90c007310feee5e5
Runner287 <75935153+Runner287@users.noreply.github.com>
2021-02-11 19:39:36 +0530

    Delete commands directory

--- commands/info/ping.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: "ping",
-    category: "info",
-    description: "Returns latency and API ping",
-    run: async (client, message, args) => {
-       message.channel.send(`Pong - ${client.ws.ping}ms`)
-    }
-}
